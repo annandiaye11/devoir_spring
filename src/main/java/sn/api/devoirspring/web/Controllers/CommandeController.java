6ae8236e61fbe21bc522088c4c615fc76f622208
annandiaye11 <anna.ndiaye1@ism.edu.sn>
2025-01-20 12:33:31 +0000

    done now

--- src/main/java/sn/api/devoirspring/web/Controllers/CommandeController.java
+++ src/main/java/sn/api/devoirspring/web/Controllers/CommandeController.java
@@ -7,6 +7,9 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import sn.api.devoirspring.data.entities.Commande;
 import sn.api.devoirspring.services.CommandeService;
+import sn.api.devoirspring.web.DTO.response.CommandeResponseDTO;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/commande")
@@ -18,14 +21,14 @@ public class CommandeController {
     }
 
     @GetMapping("/client/{clientId}")
-    public ResponseEntity<List<CommandeDTO>> getCommandesByClient(@PathVariable Long clientId) {
+    public ResponseEntity<List<CommandeResponseDTO>> getCommandesByClient(@PathVariable Long clientId) {
         return ResponseEntity.ok(commandeService.getCommandesByClientId(clientId));
     }
 
     @PostMapping("/client/{clientId}")
     public ResponseEntity<Commande> addCommande(
             @PathVariable Long clientId,
-            @Validated @RequestBody CommandeDTO commandeDTO) {
+            @Validated @RequestBody CommandeResponseDTO commandeDTO) {
         return new ResponseEntity<>(commandeService.addCommande(clientId, commandeDTO), HttpStatus.CREATED);
     }
 }
